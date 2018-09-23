428dc8effbffdd123a1b90a7356d430c53ab40cc
freewind <nowindlee@gmail.com>
2018-09-23 11:35:25 +0800

    let -> const

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-let express = require('express');
-let app = express();
+const express = require('express')
+const app = express()
 
 app.use(express.static('./', {
-  dotfiles: 'ignore',
-  etag: true,
-  extensions: ['html', 'js', 'css'],
-  index: ['index.html'], // or `false`
-  maxAge: '1d',
-  redirect: false,
-  setHeaders: function (res, path, stat) {
-    res.set('x-timestamp', Date.now());
-  }
-}));
+    dotfiles: 'ignore',
+    etag: true,
+    extensions: ['html', 'js', 'css'],
+    index: ['index.html'], // or `false`
+    maxAge: '1d',
+    redirect: false,
+    setHeaders: function (res, path, stat) {
+        res.set('x-timestamp', Date.now())
+    }
+}))
 
-app.get('/hello', function(req, res) {
-  res.send('Hello world!');
-});
+app.get('/hello', function (req, res) {
+    res.send('Hello world!')
+})
 
-app.listen(3000, function() {
-  console.log('server is listening on 3000');
+app.listen(3000, function () {
+    console.log('server is listening on 3000')
 })
